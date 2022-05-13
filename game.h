41eb48a56aae80d83c682fb1fdd68256b78804f6
PortsyC <98475206+PortsyC@users.noreply.github.com>
2022-05-13 14:05:26 -0400

    Create game.h

---
+++ game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "board.h"
+
+class Game
+{
+public:
+	Game(int minecount, int x, int y);
+	~Game();
+	int update(int x, int y);
+private:
+	Board board;
+};
+
+#endif
